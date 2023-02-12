27f73073c6a2777b3164bebf3df84b6d9fd82a55
ehellgre <emil.hellgren@edu.lapinamk.fi>
2023-02-12 22:52:23 +0200

    components n' font setup

--- static-site/src/index.js
+++ static-site/src/index.js
@@ -1,1 +1,10 @@
+// importing images -> 'import [anything] from "./images/[image_name]" -> <img src={reactLogo} />
+
+import React from "react"
+import { createRoot } from 'react-dom/client'
 import "./style.css"
+import App from "./App"
+
+const root = createRoot(document.getElementById("root"))
+
+root.render(<App />)

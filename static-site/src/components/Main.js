3f1e1d7217437a99521f290727d40d00b4c22133
ehellgre <emil.hellgren@edu.lapinamk.fi>
2023-02-13 20:57:47 +0200

    components and styling done

--- static-site/src/components/Main.js
+++ static-site/src/components/Main.js
@@ -1,7 +1,18 @@
 import React from "react"
 
 function Main() {
-    return <h1>Main component</h1>
+    return (
+        <main>
+            <h1 className="main--fun_facts_text">Fun facts about React</h1>
+            <ul className="main--fun_facts">
+                <li> First release in 2013 </li>
+                <li> Was originally created by Jordan Walke </li>
+                <li> Has over 100K stars on GitHub </li>
+                <li> Maintained by Facebook </li>
+                <li> Powers thousands of enterprise apps </li>
+            </ul>
+        </main>
+    )
 }
 
 export default Main
--- static-site/src/components/Navbar.js
+++ static-site/src/components/Navbar.js
@@ -1,7 +1,14 @@
 import React from "react";
+import reactLogo from "../images/logo192.png"
 
 function Navbar() {
-    return <h1>Navbar</h1>
+    return (
+        <nav>
+            <img src={reactLogo} className="nav--icon" />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Proj 1</h4>
+        </nav>
+    )
 }
 
 export default Navbar
